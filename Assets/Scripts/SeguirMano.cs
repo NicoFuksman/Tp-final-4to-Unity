d5c01ef2dde0b9742eeda39f8fd2b2a7cdbcb189
NicoFuksman <bf0005@est.ort.edu.ar>
2025-11-20 11:24:34 -0300

    cancha y pelotas agregadas y va a la mano

---
+++ Assets/Scripts/SeguirMano.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirMano : MonoBehaviour
+{
+    private Transform mano;
+
+    void Start()
+    {
+        // Busca automáticamente un objeto en la escena llamado "mano"
+        GameObject manoObj = GameObject.Find("mano");
+
+        if (manoObj != null)
+        {
+            mano = manoObj.transform;
+        }
+        else
+        {
+            Debug.LogError("No se encontró un objeto llamado 'mano' en la escena.");
+        }
+    }
+
+    void Update()
+    {
+        if (mano != null)
+        {
+            // Igualar la posición de la pelota a la del objeto mano
+            transform.position = mano.position;
+        }
+    }
+}
