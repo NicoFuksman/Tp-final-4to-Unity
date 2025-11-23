67792823438e41a9a74f746953178d154ee1ecc1
NicoFuksman <bf0005@est.ort.edu.ar>
2025-11-23 01:33:05 -0300

    diferentes carros y base agregada

---
+++ Assets/Scripts/basescript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basescript : MonoBehaviour
+{
+    public bool estarquieto = false;
+    private Transform objetoAQuietar;
+
+    void Update()
+    {
+        if (estarquieto && objetoAQuietar != null)
+        {
+            objetoAQuietar.position = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // si querés que funcione SOLO con el jugador:
+        if (other.gameObject.name == "SimpleFPSController")
+        {
+            objetoAQuietar = other.transform;
+            estarquieto = true;
+        }
+    }
+}
